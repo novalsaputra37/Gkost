7fdafbf7abf3aacf2ccb5ef394c8ab4fad609598
Novalsaputra37 <novalsaputra37@gmail.com>
2022-03-20 21:49:05 +0700

    20-march

--- core/settings.py
+++ core/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gkost',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'DEhidara1220',
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
     }
